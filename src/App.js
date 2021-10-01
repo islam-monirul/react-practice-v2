bf7ed612533af94309cb8fc7e2b131cdb9e4091c
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-10-01 20:32:15 +0600

    epl api fetched

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Teams></Teams>
+    	</div>
+  	);
+}
+
+function Teams(){
+	const [teams, setTeams] = useState([]);
+
+	useEffect( () => {
+		fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League')
+			.then(res => res.json())
+				.then(data => setTeams(data.teams));
+	},[]);
+
+	// console.log(teams.length);
+
+	return (
+		<div>
+			<h2>English Premier League</h2>
+		</div>
+	)
 }
 
 export default App;
